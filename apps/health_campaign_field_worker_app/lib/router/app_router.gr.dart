1c140778d0700333ec08f21647efe906338320c8
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-11-10 15:19:07 +0530

    HLM-2979::Data Report change in Success Screen and In Progress dialog changes

--- apps/health_campaign_field_worker_app/lib/router/app_router.gr.dart
+++ apps/health_campaign_field_worker_app/lib/router/app_router.gr.dart
@@ -152,6 +152,7 @@ class _$AppRouter extends RootStackRouter {
           isDataRecordSuccess: args.isDataRecordSuccess,
           label: args.label,
           description: args.description,
+          descriptionTableData: args.descriptionTableData,
         ),
       );
     },
@@ -1332,6 +1333,7 @@ class AcknowledgementRoute extends PageRouteInfo<AcknowledgementRouteArgs> {
     bool isDataRecordSuccess = false,
     String? label,
     String? description,
+    Map<String, dynamic>? descriptionTableData,
   }) : super(
           AcknowledgementRoute.name,
           path: 'acknowledgement',
@@ -1341,6 +1343,7 @@ class AcknowledgementRoute extends PageRouteInfo<AcknowledgementRouteArgs> {
             isDataRecordSuccess: isDataRecordSuccess,
             label: label,
             description: description,
+            descriptionTableData: descriptionTableData,
           ),
         );
 
@@ -1354,6 +1357,7 @@ class AcknowledgementRouteArgs {
     this.isDataRecordSuccess = false,
     this.label,
     this.description,
+    this.descriptionTableData,
   });
 
   final Key? key;
@@ -1366,9 +1370,11 @@ class AcknowledgementRouteArgs {
 
   final String? description;
 
+  final Map<String, dynamic>? descriptionTableData;
+
   @override
   String toString() {
-    return 'AcknowledgementRouteArgs{key: $key, appLocalizations: $appLocalizations, isDataRecordSuccess: $isDataRecordSuccess, label: $label, description: $description}';
+    return 'AcknowledgementRouteArgs{key: $key, appLocalizations: $appLocalizations, isDataRecordSuccess: $isDataRecordSuccess, label: $label, description: $description, descriptionTableData: $descriptionTableData}';
   }
 }
 
