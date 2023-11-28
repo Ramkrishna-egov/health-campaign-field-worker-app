83422d132ff7ddf37f6a0486871df9f9e6e58e20
amitverma-egov <amit.verma@egovernments.org>
2023-11-28 12:41:53 +0530

    minLength validation text fix

--- apps/health_campaign_field_worker_app/lib/utils/utils.dart
+++ apps/health_campaign_field_worker_app/lib/utils/utils.dart
@@ -71,7 +71,7 @@ class CustomValidator {
             control.value.toString().trim().length >= 3 ||
             control.value.toString().trim().isEmpty
         ? null
-        : {'mín3': true};
+        : {'min3': true};
   }
 
   static Map<String, dynamic>? validMobileNumber(
