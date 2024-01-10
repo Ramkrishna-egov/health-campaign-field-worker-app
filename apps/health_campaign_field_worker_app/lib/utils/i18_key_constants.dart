60f99cd21d6e6eae4782f6939b6892c10ddb630b
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2024-01-10 18:55:12 +0530

    duplicate value changed

--- apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
@@ -114,8 +114,6 @@ class Common {
 
   String get coreCommonDownloadFailed => 'CORE_COMMON_DOWNLOAD_FAILED';
 
-  String get noMatchFound => 'CORE_COMMON_NO_MATCH_FOUND';
-
   String get scanBales => 'CORE_COMMON_SCAN_BALES';
 }
 
