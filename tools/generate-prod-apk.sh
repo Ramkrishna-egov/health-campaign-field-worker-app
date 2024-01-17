405946ef52d2a8ed2cddfe13000d2f19bcc44390
vishal <sarasani.vishalreddy@egovernments.org>
2024-01-17 13:01:58 +0530

    set up of firebase and script changes for the same

--- tools/generate-prod-apk.sh
+++ tools/generate-prod-apk.sh
@@ -1,7 +1,7 @@
 cd apps/health_campaign_field_worker_app || exit
 
 
-flutter pub run change_app_package_name:main org.egov.salama.smc
+flutter pub run change_app_package_name:main org.egov.lb
 
 rm -rf release-apk
 mkdir release-apk
