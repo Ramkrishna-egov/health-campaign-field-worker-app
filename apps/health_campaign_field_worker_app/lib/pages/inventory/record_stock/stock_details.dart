0d2cee3351c44d2deed5740a0c75e2666ff7f916
vishal <sarasani.vishalreddy@egovernments.org>
2024-01-18 16:43:06 +0530

    commenting out bale scanning feature

--- apps/health_campaign_field_worker_app/lib/pages/inventory/record_stock/stock_details.dart
+++ apps/health_campaign_field_worker_app/lib/pages/inventory/record_stock/stock_details.dart
@@ -460,10 +460,9 @@ class _StockDetailsPageState extends LocalizedState<StockDetailsPage> {
                                 menuItems: reasons ?? [],
                                 formControlName: _transactionReasonKey,
                                 valueMapper: (value) {
-
                                   return localizations.translate(
-                                        value.name,);
-                                
+                                    value.name,
+                                  );
                                 },
                                 isRequired: true,
                               ),
@@ -620,24 +619,26 @@ class _StockDetailsPageState extends LocalizedState<StockDetailsPage> {
                                     ),
                               },
                             ),
-                            DigitOutlineIconButton(
-                              buttonStyle: OutlinedButton.styleFrom(
-                                shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.zero,
-                                ),
-                              ),
-                              onPressed: () {
-                                context.router.push(QRScannerRoute(
-                                  quantity: 5,
-                                  isGS1code: true,
-                                  sinlgleValue: false,
-                                ));
-                              },
-                              icon: Icons.qr_code,
-                              label: localizations.translate(
-                                i18.common.scanBales,
-                              ),
-                            ),
+                            // Commenting this because we need this functionality for other APK
+
+                            // DigitOutlineIconButton(
+                            //   buttonStyle: OutlinedButton.styleFrom(
+                            //     shape: const RoundedRectangleBorder(
+                            //       borderRadius: BorderRadius.zero,
+                            //     ),
+                            //   ),
+                            //   onPressed: () {
+                            //     context.router.push(QRScannerRoute(
+                            //       quantity: 5,
+                            //       isGS1code: true,
+                            //       sinlgleValue: false,
+                            //     ));
+                            //   },
+                            //   icon: Icons.qr_code,
+                            //   label: localizations.translate(
+                            //     i18.common.scanBales,
+                            //   ),
+                            // ),
                           ],
                         ),
                       ),
