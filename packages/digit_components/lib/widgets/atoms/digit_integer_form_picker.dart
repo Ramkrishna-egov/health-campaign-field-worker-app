816ead00768e4a7136b98b7d2f809e0dfb9c1012
vishal <sarasani.vishalreddy@egovernments.org>
2024-02-05 19:13:24 +0530

    max limit for integer increment

--- packages/digit_components/lib/widgets/atoms/digit_integer_form_picker.dart
+++ packages/digit_components/lib/widgets/atoms/digit_integer_form_picker.dart
@@ -64,14 +64,23 @@ class DigitIntegerFormPicker extends StatelessWidget {
                   keyboardType: TextInputType.number,
                 ),
               ),
-              _buildButton(context,
-                  border: Border(
-                    right: _borderSide,
-                    bottom: _borderSide,
-                    top: _borderSide,
-                  ),
-                  icon: Icons.add,
-                  onPressed: () => form.control(formControlName).value += 1),
+              _buildButton(
+                context,
+                border: Border(
+                  right: _borderSide,
+                  bottom: _borderSide,
+                  top: _borderSide,
+                ),
+                icon: Icons.add,
+                onPressed: () {
+                  maximum != null
+                      ? form.control(formControlName).value < maximum ||
+                              form.control(formControlName).value == null
+                          ? form.control(formControlName).value += 1
+                          : maximum
+                      : form.control(formControlName).value += 1;
+                },
+              ),
             ],
           ),
         ));
